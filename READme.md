ecbed75a162fbb2115b3f88dc725823b101d9d53
Jennifer Bell <student@epicodus.com>
2015-08-04 10:55:26 -0700

    fix formatting and update README

--- READme.md
+++ READme.md
@@ -3,4 +3,6 @@
 # Scrabble takes a word and returns the scrabble score for that word
 
 1. Clone the repo
-2. Run 
+2. `cd` into the directory
+3. Run using `ruby lib/scrabble.rb`
+4. Test (Rspec) using `rspec`
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,12 +2,12 @@ require("rspec")
 require("scrabble")
 
 describe("String#scrabble") do
-  it("returns an score for a letter") do
+  it("returns a score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
 
-    it("returns an score for multiple letters") do
-      expect("za".scrabble()).to(eq(11))
+  it("returns a score for multiple letters") do
+    expect("za".scrabble()).to(eq(11))
   end
 
   it("handles lower or uppercase letters") do
